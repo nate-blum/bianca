10db48b8667928457699b662bb641a37b94f7b7c
nate-blum <nate.blum13@gmail.com>
2025-01-08 19:05:19 -0500

    added nasm and ld

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod helper;
 mod codegen;
 
 use std::fs;
+use std::process::Command;
 
 fn main() {
     let file_path = "test.bky"; // replace with args
@@ -14,6 +15,17 @@ fn main() {
     let prog = parser::parse(tokens);
     let code = codegen::codegen(prog);
 
-    let out_file = "test.asm";
+    let out_file = "out.asm";
     fs::write(out_file, code).expect("Could not write file");
+    // check with nasm and stuff
+
+    Command::new("nasm")
+        .args(["-felf64", "out.asm"])
+        .output()
+        .expect("Failed to execute nasm");
+
+    Command::new("ld")
+        .args(["-o", "out", "out.o"])
+        .output()
+        .expect("Failed to execute ld");
 }
